28f5978e405a89dc71fc4e4018f64df5edd99876
Mikhail Nasyrov <mnasyrov@xored.com>
2013-05-22 16:36:42 +0700

    Added jquery. Reworked waiting for a message div. Fixed a size of diff text.

--- src/gcolordiff.js
+++ src/gcolordiff.js
@@ -1,35 +1,52 @@
-(function() {
+(function($) {
 
-	var style = 
-		'<style type="text/css"> ' +
-		'.gcolordiff-message { font-family: monospace; }' +
+	/**
+	* @function
+	* @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+	* @param {function} handler A function to execute at the time when the element is inserted
+	* @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+	* @example $(selector).waitUntilExists(function);
+	*/
 
-		'.gcolordiff-summary-author, .gcolordiff-summary-date, .gcolordiff-summary-newrevision, ' +
-		'.gcolordiff-summary-modified, .gcolordiff-summary-added, .gcolordiff-summary-deleted, ' +
-		'.gcolordiff-summary-log, .gcolordiff-summary-url ' + 
-		'{ font-weight: bold; text-decoration: underline; } ' +
+	$.fn.waitUntilExists = function (handler, shouldRunHandlerOnce, isChild) {
+		var found = 'found';
+		var $this = $(this.selector);
+		var $elements = $this.not(function() { return $(this).data(found); }).each(handler); //.data(found, true);
 
-		'.gcolordiff-summary-separator { color: lightgrey; } ' + 
+		if (!isChild){
+	    	(window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+				window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500);
+		}
+		else if (shouldRunHandlerOnce && $elements.length) {
+			window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+		}
 
-		'.gcolordiff-diff-oldrev { color:red;font-weight:bold } ' + 
-		'.gcolordiff-diff-newrev { color:green;font-weight:bold } ' +
-		'.gcolordiff-diff-lineindex { color: blue; } ' +
-		'.gcolordiff-diff-oldline { color: red; } ' +
-		'.gcolordiff-diff-newline { color: green; } ' +
-		'</style>';
+		return $this;
+	}
 
-	function gcolordiff() {
-		var div = getMessageContainer();
-		if (div && isDiffMessage(div.innerText)) {
-			div.parentNode.style.fontFamily = "monospace";
+
+	function colordiff() {
+		var $div = $(this)[0];
+		if ($(div).hasClass('gcolordiff-message')) {
+			return;
+		}
+
+		var div = $(this)[0];
+		if (div) {
+			var html = div.innerHTML;
+			if (!isDiffMessage(html)) {
+				return;
+			}
+
+			$(div).addClass('gcolordiff-message');
 			
 			var oldRevRegExp = /\n(--- .*)(<br\/?>)/g;
 			var newRevRegExp = /\n(\+\+\+ .*)(<br\/?>)/g;
 			var lineIndexRegExp = /\n(@@.*@@)/g;
 			var oldRevLineRegExp = /\n(-.*)(<br\/?>)/g;
 			var newRevLineRegExp = /\n(\+.*)(<br\/?>)/g;
-
-			div.innerHTML = div.innerHTML
+			
+			html = html
 			.replace(/(Author:)/g, '\n<span class="gcolordiff-summary-author">$1</span>')
 			.replace(/\n(Date:)/g, '\n<span class="gcolordiff-summary-date">$1</span>')
 			.replace(/\n(New Revision:)/g, '\n<span class="gcolordiff-summary-newrevision">$1</span>')
@@ -44,36 +61,18 @@
 			.replace(lineIndexRegExp, '\n<span class="gcolordiff-diff-lineindex">$1</span>')
 			.replace(oldRevLineRegExp, '\n<span class="gcolordiff-diff-oldline">$1</span>$2')
 			.replace(newRevLineRegExp, '\n<span class="gcolordiff-diff-newline">$1</span>$2');
+			
+			div.innerHTML = html;
 		}
 	}	
-
-	function getMessageContainer() {
-		var elements = document.getElementsByClassName("ii gt");
-		if (elements.length == 0) {
-			elements = document.getElementsByClassName("message");
-		}
-		if (elements.length == 0) {
-			elements = document.getElementsByClassName("msg");
-		}
-		if (elements.length > 0) {
-		    return elements[0];
-	    }
-	    return null;
-	}
 
 	function isDiffMessage(str) {
 		return str && (str.indexOf('- Log -------------') >= 0 || str.indexOf('Changes diff:') >= 0 || str.indexOf('diff --git') >= 0);
 	}
 
-	function handler() {
-		setTimeout(gcolordiff, 1000);
-	}
-
-	window.addEventListener('hashchange', handler);
-	window.addEventListener('load', handler);
-	document.addEventListener('DOMContentLoaded', handler);
-
 	$(function() {
-		$('head').append(style);
+		$('.ii.gt, .message, .msg').waitUntilExists(colordiff);
 	})
-})();
+
+
+})(jQuery);
